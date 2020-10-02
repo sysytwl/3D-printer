2a3464c6a9e5acb82171caf570c9f22ab7eec9a1
JackShenYt <61526569+sysytwl@users.noreply.github.com>
2020-10-02 10:01:28 +0800

    update

---
+++ drivers & software/Untitled-1.java
@@ -0,0 +1,15 @@
+public class jack{
+public void rose(){
+System.out.printIn("love");
+}
+}
+
+class jack2 extends jack{
+    public void rose(){
+        System.out.printIn("love2")
+    }
+    public static void main(String args[]){
+        jack a = new jack2():
+        a.rose();
+    }
+}
